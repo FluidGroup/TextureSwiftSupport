d17b9455b0ddf8000943eb0e5c9d7419e91b02c5
LiuJian <strangeliu@gmail.com>
2021-01-29 01:18:01 +0800

    Add AbsoluteLayout (#43)

--- TextureSwiftSupport/SpecBuilder/SpecBuilder.swift
+++ TextureSwiftSupport/SpecBuilder/SpecBuilder.swift
@@ -402,6 +402,26 @@ public struct WrapperLayout<Content> : _ASLayoutElementType where Content : _ASL
 
 ///
 /// - Author: TetureSwiftSupport
+public struct AbsoluteLayout<Content> : _ASLayoutElementType where Content : _ASLayoutElementType {
+  
+  public let sizing: ASAbsoluteLayoutSpecSizing
+  public let children: Content
+  
+  public init(
+    sizing: ASAbsoluteLayoutSpecSizing = .default,
+    @ASLayoutSpecBuilder content: () -> Content
+  ) {
+    self.sizing = sizing
+    self.children = content()
+  }
+  
+  public func make() -> [ASLayoutElement] {
+    [ASAbsoluteLayoutSpec(sizing: sizing, children: children.make())]
+  }
+}
+
+///
+/// - Author: TetureSwiftSupport
 public struct CenterLayout<Content> : _ASLayoutElementType where Content : _ASLayoutElementType {
   
   public let content: Content
