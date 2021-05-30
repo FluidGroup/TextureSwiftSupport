65c8686176b360ecf560dde2f18e5d9b63b10790
Muukii <muukii.app@gmail.com>
2021-05-31 01:19:39 +0900

    Add Haptics feature #59

---
+++ Demo/Book.InteractiveNode.swift
@@ -0,0 +1,89 @@
+import StorybookKit
+import StorybookKitTextureSupport
+import TextureSwiftSupport
+
+extension Book {
+
+  private static func makeBody() -> ASDisplayNode {
+    AnyDisplayNode { node, _ in
+      LayoutSpec {
+        node._makeNode {
+          let textNode = ASTextNode()
+          textNode.attributedText = NSAttributedString(string: "InteractiveNode")
+          return textNode
+        }
+        .padding(8)
+        .background(
+          node._makeNode {
+            ShapeLayerNode.roundedCorner(radius: 16).setShapeFillColor(.systemYellow)
+          }
+        )
+      }
+    }
+  }
+
+  static func bookInteractiveNode() -> BookView {
+    BookNavigationLink(title: "InteractiveNode") {
+      BookNodePreview {
+        InteractiveNode(animation: .translucent) {
+          makeBody()
+        }
+      }
+
+      BookSection(title: "With Haptics") {
+
+        BookForEach(data: [.light, .medium, .heavy] as [UIImpactFeedbackGenerator.FeedbackStyle]) {
+          style in
+          BookNodePreview {
+            InteractiveNode(
+              animation: .translucent,
+              haptics: .impactOnTouchUpInside(style: style)
+            ) {
+              makeBody()
+            }
+          }
+          .title("Impact style:\(style.localizedDescription)")
+        }
+
+        if #available(iOS 13, *) {
+
+          BookForEach(data: [.soft, .rigid] as [UIImpactFeedbackGenerator.FeedbackStyle]) {
+            style in
+            BookNodePreview {
+              InteractiveNode(
+                animation: .translucent,
+                haptics: .impactOnTouchUpInside(style: style)
+              ) {
+                makeBody()
+              }
+            }
+            .title("Impact style:\(style.localizedDescription)")
+          }
+
+        }
+      }
+
+    }
+  }
+}
+
+extension UIImpactFeedbackGenerator.FeedbackStyle {
+
+  fileprivate var localizedDescription: String {
+    switch self {
+    case .light:
+      return "light"
+    case .medium:
+      return "medium"
+    case .heavy:
+      return "heavy"
+    case .soft:
+      return "soft"
+    case .rigid:
+      return "rigid"
+    @unknown default:
+      return "unknown"
+    }
+  }
+
+}
