d554ba6835b92b427eef85a5ac2ae77a6f644d90
Muukii <muukii.app@gmail.com>
2022-07-01 02:45:58 +0900

    Add Package.swift

--- Package.swift
+++ Package.swift
@@ -10,13 +10,15 @@ let package = Package(
     .library(name: "TextureSwiftSupport", targets: ["TextureSwiftSupport"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/3a4oT/Texture.git", branch: "spm"),
+    .package(url: "https://github.com/FluidGroup/Texture.git", branch: "spm"),
+    .package(url: "https://github.com/FluidGroup/Descriptors", from: "0.2.0"),
   ],
   targets: [
     .target(
       name: "TextureSwiftSupport", 
       dependencies: [
-        .product(name: "AsyncDisplayKit", package: "Texture")
+        .product(name: "AsyncDisplayKit", package: "Texture"),
+        .product(name: "Descriptors", package: "Descriptors")
       ],
       path: "Sources"
     ),
